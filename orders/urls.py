3d1ec5d321e981f48ebc6387b956afce65490504
gekk0 <gekk0dw@gmail.com>
2022-08-27 19:19:24 +0300

    customer's orders history

--- orders/urls.py
+++ orders/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.order_create, name='order_create'),
     path("account_details", views.account_details, name="account_details"),
-    path("edit_account_details", views.edit_account_details, name="edit_account_details")
+    path("edit_account_details", views.edit_account_details, name="edit_account_details"),
+    path("customer_orders", views.customer_orders, name="customer_orders")
 ]
