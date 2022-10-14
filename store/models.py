31770b0be2d32179e5b62eeb469b0564a0b0e215
gekk0 <gekk0dw@gmail.com>
2022-10-14 12:13:07 +0300

    media + upload_to changes

--- store/models.py
+++ store/models.py
@@ -6,7 +6,7 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 class Categories(models.Model):
     name = models.CharField(max_length=128, verbose_name="Название категории")
-    image = models.ImageField(verbose_name="Обложка", upload_to="media/category_covers", default="media/no-image.png")
+    image = models.ImageField(verbose_name="Обложка", upload_to="category_covers", default="no-image.png")
     hidden = models.BooleanField(verbose_name="Только для избранных", default=False)
 
     def __str__(self):
@@ -22,7 +22,7 @@ class Items(models.Model):
     category = models.ForeignKey(Categories, on_delete=models.CASCADE, verbose_name="Относится к категории:")
     price = models.IntegerField(verbose_name="Цена", default=0)
     description = models.TextField(verbose_name="Описание товара", null=True, blank=True)
-    main_image = models.ImageField(verbose_name="Основное изображение", upload_to="media/item_covers", default="media/no-image.png")
+    main_image = models.ImageField(verbose_name="Основное изображение", upload_to="item_covers", default="no-image.png")
 
     def __str__(self):
         return self.name
@@ -33,7 +33,7 @@ class Items(models.Model):
 
 
 class ItemImages(models.Model):
-    image = models.ImageField(verbose_name="Изображение", upload_to="media", default="no-image.png")
+    image = models.ImageField(verbose_name="Изображение", upload_to="", default="no-image.png")
     of_item = models.ForeignKey(Items, on_delete=models.CASCADE, verbose_name="Относится к товару:")
     caption = models.CharField(max_length=256, verbose_name="Подпись", null=True, blank=True)
 
